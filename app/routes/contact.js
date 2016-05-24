c00faea66b0bc90f3487757d0bdf0de6035a2d7a
lordik8066 <lordik8066096@gmail.com>
2016-05-24 09:19:23 +0300

    lesson 4

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,7 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model() {
+        return this.store.createRecord('contact');
+    },
 
-
-
+    actions: {
+        saveContact(newContact) {
+            newContact.save().then(() => {
+                this.transitionTo('admin.contacts');
+            });
+        },
+        willTransition() {
+            this.controller.get('model').rollbackAttributes();
+        }
+    }
 });
